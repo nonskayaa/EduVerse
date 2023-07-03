91f3a430ef125843ca949e3b45c5f0ed82d06902
nonskayaa <nonskaya2003@bk.ru>
2023-07-03 14:52:03 +0300

    added Modules

--- myCourses/courses/urls.py
+++ myCourses/courses/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from .import views
 
 urlpatterns = [
      path('mine/',
@@ -13,5 +13,21 @@ urlpatterns = [
           name='course_edit'),
      path('<pk>/delete/',
           views.CourseDeleteView.as_view(),
-          name='course_delete')
+          name='course_delete'),
+     path('<pk>/module/',
+          views.CourseModuleUpdateView.as_view(),
+          name='course_module_update'),
+     path('module/<int:module_id>/content/<model_name>/create/',
+          views.ContentCreateUpdateView.as_view(),
+          name='module_content_create'),
+     path('module/<int:module_id>/content/<model_name>/<id>/',
+          views.ContentCreateUpdateView.as_view(),
+          name='module_content_update'),
+     path('content/<int:id>/delete/',
+          views.ContentDeleteView.as_view(),
+          name='module_content_delete'),
+     path('module/<int:module_id>/',
+          views.ModuleContentListView.as_view(),
+          name='module_content_list'),
+
 ]
