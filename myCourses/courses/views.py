fe89c74023507bec7ca1b1c8320321bd53dd1ab6
1chess <1chessMic@gmail.com>
2023-07-08 12:29:16 +0300

    Жёсткая вёрстка

--- myCourses/courses/views.py
+++ myCourses/courses/views.py
@@ -46,9 +46,11 @@ class CourseListView(TemplateResponseMixin, View):
                         total_courses=Count('courses'))
         courses = Course.objects.annotate(
                         total_modules=Count('modules'))
+
         if subject:
             subject = get_object_or_404(Subject, slug=subject)
             courses = courses.filter(subject=subject)
+
         return self.render_to_response({'subjects': subjects,
                                         'subject': subject,
                                         'courses': courses})
